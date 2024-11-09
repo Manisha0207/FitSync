e9f66c1a8998868d72a4aaf749101e9b6cd91fe2
John Doe <john.doe@contoso.com>
2024-11-09 23:12:24 +0530

    vercel

--- server/index.js
+++ server/index.js
@@ -455,6 +455,6 @@ app.get('/', (req, res) => {
 });
 
 // Start the server
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on http://localhost:${process.env.PORT}`);
 });
